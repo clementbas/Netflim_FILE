dd882e675ae14121ee187d02d1ccf98261f5a89b
Clément BAS <123475765+clementbas@users.noreply.github.com>
2026-02-09 15:05:54 +0100

    Add directory creation for file uploads in app initialization

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 import { env } from './config/env.js';
 import fileRoutes from './routes/file.routes.js';
 import { errorHandler } from './middlewares/error.middleware.js';
@@ -8,6 +9,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const baseUploadDir = env.uploadDir;
+const requiredUploadDirs = [
+  baseUploadDir,
+  `${baseUploadDir}/images`,
+  `${baseUploadDir}/videos`
+];
+
+requiredUploadDirs.forEach((dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+});
 
 app.use('/files', fileRoutes);
 app.use(errorHandler);
